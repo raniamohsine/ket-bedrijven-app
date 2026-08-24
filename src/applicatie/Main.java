422f69ae379b4e2576ea7bfc7d32552df12aa0d2
raniamohsine <raniamohsine07@gmail.com>
2026-08-24 21:47:12 +0200

    add application menu

--- src/applicatie/Main.java
+++ src/applicatie/Main.java
@@ -8,24 +8,46 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("KET bedrijven applicatie");
-
         BedrijfDienst bedrijfDienst = new BedrijfDienst();
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Geef een zoekterm: ");
-        String zoekterm = scanner.nextLine();
+        boolean actief = true;
 
-        ArrayList<Bedrijf> resultaten = bedrijfDienst.zoekBedrijven(zoekterm);
+        while (actief) {
+            System.out.println("\nKET bedrijven applicatie");
+            System.out.println("1. Bedrijven zoeken");
+            System.out.println("2. Afsluiten");
+            System.out.print("Maak een keuze: ");
 
-        if (resultaten.isEmpty()) {
-            System.out.println("Geen bedrijven gevonden.");
-        } else {
-            System.out.println("Zoekresultaten:");
+            String keuze = scanner.nextLine();
 
-            for (Bedrijf bedrijf : resultaten) {
-                System.out.println("--------------------");
-                System.out.println(bedrijf);
+            switch (keuze) {
+                case "1":
+                    System.out.print("Geef een zoekterm: ");
+                    String zoekterm = scanner.nextLine();
+
+                    ArrayList<Bedrijf> resultaten = bedrijfDienst.zoekBedrijven(zoekterm);
+
+                    if (resultaten.isEmpty()) {
+                        System.out.println("Geen bedrijven gevonden.");
+                    } else {
+                        System.out.println("Zoekresultaten:");
+
+                        for (Bedrijf bedrijf : resultaten) {
+                            System.out.println("--------------------");
+                            System.out.println(bedrijf);
+                        }
+                    }
+                    break;
+
+                case "2":
+                    System.out.println("Applicatie wordt afgesloten.");
+                    actief = false;
+                    break;
+
+                default:
+                    System.out.println("Ongeldige keuze. Probeer opnieuw.");
+                    break;
             }
         }
 
